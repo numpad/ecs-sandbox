33bddd04f66616389947bcccb32d4cd61eed2c6a
numpad <christian@tuta.io>
2021-01-10 23:47:08 +0100

    [build-vcpkg] Fixed build to link libraries static

--- src/ecs/components.hpp
+++ src/ecs/components.hpp
@@ -1,13 +1,13 @@
-// 3263cf3501b420c6e17ac39db9544a4214ba6d87
+// 37d95ce3088c7f5e96ebd143e2c36ea82a7db57f
 #pragma once
 
-#include <ecs/components/CVelocity.hpp>
-#include <ecs/components/CSphereCollider.hpp>
-#include <ecs/components/CGravity.hpp>
-#include <ecs/components/CPosition.hpp>
-#include <ecs/components/CBillboard.hpp>
-#include <ecs/components/CJumpTimer.hpp>
-#include <ecs/components/CSpawnPoint.hpp>
-#include <ecs/components/CKeyboardControllable.hpp>
-#include <ecs/components/CRunningToTarget.hpp>
-#include <ecs/components/CHealth.hpp>
+#include <ecs\components\CBillboard.hpp>
+#include <ecs\components\CGravity.hpp>
+#include <ecs\components\CHealth.hpp>
+#include <ecs\components\CJumpTimer.hpp>
+#include <ecs\components\CKeyboardControllable.hpp>
+#include <ecs\components\CPosition.hpp>
+#include <ecs\components\CRunningToTarget.hpp>
+#include <ecs\components\CSpawnPoint.hpp>
+#include <ecs\components\CSphereCollider.hpp>
+#include <ecs\components\CVelocity.hpp>
--- src/ecs/events.hpp
+++ src/ecs/events.hpp
@@ -1,8 +1,8 @@
-// 41613fb313cb338c35a8232ad22eb2fb22f4e885
+// e81ed3267871c9e80731ebe8426def4e891500a3
 #pragma once
 
-#include <ecs/events/KillEntityEvent.hpp>
-#include <ecs/events/DrawPrimitiveEvent.hpp>
-#include <ecs/events/LogEvent.hpp>
-#include <ecs/events/PlaySoundEvent.hpp>
-#include <ecs/events/WorldTextEvent.hpp>
+#include <ecs\events\DrawPrimitiveEvent.hpp>
+#include <ecs\events\KillEntityEvent.hpp>
+#include <ecs\events\LogEvent.hpp>
+#include <ecs\events\PlaySoundEvent.hpp>
+#include <ecs\events\WorldTextEvent.hpp>
