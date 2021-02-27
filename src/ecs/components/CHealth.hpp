bd7b11f95dbd2b0b97821f46101d9b0fdec991d6
Christian Schäl <christian.schael87@gmail.com>
2021-02-27 11:03:31 +0100

    [52] Cleaning up entity picker

--- src/ecs/components/CHealth.hpp
+++ src/ecs/components/CHealth.hpp
@@ -3,6 +3,7 @@
 struct CHealth {
 	int hp, max_hp;
 	
+	CHealth() : hp(1), max_hp(1) {}
 	CHealth(int hp) : hp(hp), max_hp(hp) {}
 	
 };
--- src/ecs/components/COrientation.hpp
+++ src/ecs/components/COrientation.hpp
@@ -6,6 +6,11 @@ struct COrientation {
 	glm::vec3 orientation;
 	float amount;
 	
+	COrientation()
+		: orientation(glm::vec3(0.f, -1.f, 0.f)), amount(1.f)
+	{
+	}
+
 	COrientation(glm::vec3 orientation, float amount)
 		: orientation(orientation), amount(amount)
 	{
