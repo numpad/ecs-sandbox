bc325f7d9c24bbbb5c4a8b22cf9f6e001ebaec02
Christian Schäl <christian.schael87@gmail.com>
2021-06-19 02:09:56 +0200

    [58] Small edge-case fix for angle

--- src/ecs/components/COrientedTexture.hpp
+++ src/ecs/components/COrientedTexture.hpp
@@ -8,6 +8,12 @@ struct COrientedTexture {
 	float angle;
 
 	int get_direction() const {
-		return int((angle / (glm::pi<float>() * 2.f)) * directions);
+		float rad = angle;
+		int deg = int(glm::degrees(angle));
+		if (glm::abs(deg) == 45) {
+			rad += glm::sign(deg) * 0.4f;
+		}
+
+		return int((rad / (glm::pi<float>() * 2.f)) * directions);
 	}
 };
